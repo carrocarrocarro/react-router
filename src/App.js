d22c7158bd32385982a69e6377b85416c791ae96
carrocarrocarro <52441064+carrocarrocarro@users.noreply.github.com>
2023-02-28 21:44:40 +0100

    Project for learning React Router for Scrimba.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Vans from './pages/Vans'
+import './app.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link to="/" className="site-logo">#VANLIFE</Link>
+        <nav>
+        <Link to="/about">About</Link>
+        <Link to="/vans">Vans</Link>
+        </nav>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />}/>{/* / */}
+        <Route path="/about" element={<About />}/>
+        <Route path="/vans" element={<Vans />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Vans.js
@@ -0,0 +1,10 @@
+function Vans() {
+    return (
+      <div className="Vans">
+        <h1>Vans</h1>
+      </div>
+    );
+  }
+  
+  export default Vans;
+  
